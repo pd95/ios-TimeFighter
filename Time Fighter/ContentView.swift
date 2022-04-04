7c919f19bc5c923878d2ebe47e125adeb0b3f282
Philipp <pd95@users.noreply.github.com>
2022-04-04 10:40:58 +0200

    Making the basic game and UI work

--- Time Fighter/ContentView.swift
+++ Time Fighter/ContentView.swift
@@ -6,11 +6,109 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+
+    private let initialCountDown = 3
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    @State private var score = 0
+    @State private var gameStarted = false
+    @State private var timeLeft = 60
+    @State private var timerCancellable: Cancellable?
+
+    @State private var showTimeIsUpAlert = false
+    @State private var showInfoAlert = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color("backgroundColor")
+
+                VStack {
+                    HStack {
+                        Text("Your score: \(score)")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("Time Left: \(timeLeft)")
+                    }
+                    Spacer()
+                    Button(action: incrementScore) {
+                        Text("Tap Me!")
+                            .font(.title3.smallCaps())
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 8)
+                            .background(
+                                RoundedRectangle(cornerRadius: 4)
+                                    .foregroundColor(Color("buttonColor"))
+                                    .shadow(radius: 1)
+                            )
+                            .foregroundColor(.black)
+                    }
+                    Spacer()
+                }
+                .padding()
+                .alert("Time's up!", isPresented: $showTimeIsUpAlert, actions: {
+                    Button("OK") {
+                        resetGame()
+                    }
+                }, message: {
+                    Text("Your score is \(score)")
+                })
+                .alert("Time Fighter", isPresented: $showInfoAlert, actions: {
+                    Button("Dismiss") { }
+                }, message: {
+                    Text("Created by The Mighty Swift Developer")
+                })
+                .toolbar {
+                    Button {
+                        showInfoAlert.toggle()
+                    } label: {
+                        Label("Info", systemImage: "info.circle.fill")
+                            .font(.title)
+                            .imageScale(.large)
+                    }
+                    .buttonStyle(.borderless)
+                }
+            }
+            .navigationTitle("Time Fighter")
+        }
+        .onAppear(perform: resetGame)
+        .navigationViewStyle(.stack)
+    }
+
+    // MARK: Game Logic
+
+    private func resetGame() {
+        score = 0
+        timeLeft = initialCountDown
+    }
+
+    private func startGame() {
+        timerCancellable = timer.sink(receiveValue: decrementTimeLeft)
+        gameStarted = true
+    }
+
+    private func endGame() {
+        showTimeIsUpAlert = true
+        timerCancellable = nil
+        gameStarted = false
+    }
+
+    private func incrementScore() {
+        if !gameStarted {
+            startGame()
+        }
+        withAnimation {
+            score += 1
+        }
+    }
+
+    private func decrementTimeLeft(x: Timer.TimerPublisher.Output) {
+        timeLeft -= 1
+        if timeLeft == 0 {
+            endGame()
+        }
     }
 }
 
