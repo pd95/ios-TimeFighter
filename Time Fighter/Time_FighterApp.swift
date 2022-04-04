fd1bbf1f93598922a3dbcae79f7e0248f688728d
Philipp <pd95@users.noreply.github.com>
2022-04-04 10:40:26 +0200

    Initial Commit

---
+++ Time Fighter/Time_FighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_FighterApp.swift
+//  Time Fighter
+//
+//  Created by Philipp on 04.04.22.
+//
+
+import SwiftUI
+
+@main
+struct Time_FighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
